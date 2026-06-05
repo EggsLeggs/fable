563b0e565fa236da1247b42b6119f24abe43d591
amory <36281195+EggsLeggs@users.noreply.github.com>
2026-06-05 20:25:51 +0100

    fix: remove referral router import from root until router source is committed

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -4,7 +4,6 @@ import { billingRouter } from "./routers/billing";
 import { commentsRouter } from "./routers/comments";
 import { organizationRouter } from "./routers/organization";
 import { projectRouter } from "./routers/project";
-import { referralRouter } from "./routers/referral";
 import { sourceFileRouter } from "./routers/source-file";
 import { stringsRouter } from "./routers/strings";
 import { taskRouter } from "./routers/task";
@@ -18,7 +17,6 @@ export const appRouter = router({
   comments: commentsRouter,
   organization: organizationRouter,
   project: projectRouter,
-  referral: referralRouter,
   sourceFile: sourceFileRouter,
   strings: stringsRouter,
   task: taskRouter,
